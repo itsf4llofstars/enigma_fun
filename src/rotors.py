3edd06f92c7bbb2f327ee3f2fa014afaa492a311
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-19 16:35:35 -0500

    Add letters list and rotors dict

--- src/rotors.py
+++ src/rotors.py
@@ -1,4 +1,22 @@
 """rotors.py"""
 
-if __name__ == "__main__":
-    ...
+letters = (
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+)
+
+rotors_dict ={
+    "rotor_I": list(),
+    "rotor_II": list(),
+    "rotor_III": list(),
+    "rotor_IV": list(),
+    "rotor_V": list(),
+    "rotor_VI": list(),
+    "rotor_VII": list(),
+    "rotor_VIII": list(),
+}
+
+
+def create_rotor(rotor):
+    while True:
+        ...

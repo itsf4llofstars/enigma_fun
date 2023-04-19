bc57473380c5e8599f2306f0424aad0c13c25b40
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-19 16:44:45 -0500

    Add create rotors func

--- src/rotors.py
+++ src/rotors.py
@@ -1,11 +1,12 @@
 """rotors.py"""
+import random
 
-letters = (
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-)
+letters = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
 
-rotors_dict ={
+rotors_dict = {
     "rotor_I": list(),
     "rotor_II": list(),
     "rotor_III": list(),
@@ -17,6 +18,11 @@ rotors_dict ={
 }
 
 
-def create_rotor(rotor):
-    while True:
-        ...
+def create_rotors():
+    global letters, rotors_dict
+
+    for rotor, rotor_list in rotors_dict.items():
+        while len(rotors_dict[rotor]) < 26:
+            random_letter = random.choice(letters)
+            if random_letter in rotors_dict[rotor]:
+                continue

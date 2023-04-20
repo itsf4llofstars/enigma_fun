7d43f137d1972f147203e42e0b46a954128effba
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-19 23:46:18 -0500

    Add set enigma rotors method

--- src/enigma.py
+++ src/enigma.py
@@ -7,47 +7,51 @@ from collections import deque
 class Enigma:
     """Enigma Machine class"""
 
-    def __init__(self, key: str, user_rotors=None):
-        # This auto generated form PyCharm
+    def __init__(self, msg_key: str, day_key: str, user_rotors=None):
+        # Conditional auto generated form PyCharm
         if user_rotors is None:
             user_rotors = ["I", "II", "III"]
-        self.key = key
+        self.msg_key = msg_key.upper()
+        self.day_key = day_key.upper()
         self.user_rotors = user_rotors
+        self.letter = None
+        self.message = None
+
         self.keyboard = deque([
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
         ])
 
         self.rotors = {
-            "rotor_I": deque([
+            "I": deque([
                 "U", "L", "T", "A", "O", "P", "I", "Q", "F", "Z", "Y", "N", "R",
                 "M", "B", "E", "V", "X", "W", "H", "G", "D", "J", "K", "S", "C"
             ]),
-            "rotor_II": deque([
+            "II": deque([
                 "N", "Y", "L", "S", "V", "I", "M", "T", "C", "F", "Z", "G", "Q",
                 "K", "E", "R", "H", "U", "X", "B", "P", "A", "D", "J", "O", "W"
             ]),
-            "rotor_III": deque([
+            "III": deque([
                 "P", "R", "X", "Z", "A", "F", "L", "C", "O", "M", "E", "N", "S",
                 "D", "Q", "W", "J", "T", "U", "B", "K", "Y", "H", "I", "V", "G"
             ]),
-            "rotor_IV": deque([
+            "IV": deque([
                 "L", "A", "P", "M", "V", "Q", "H", "S", "U", "G", "B", "J", "T",
                 "C", "I", "D", "Y", "F", "W", "E", "O", "Z", "R", "N", "X", "K"
             ]),
-            "rotor_V": deque([
+            "V": deque([
                 "T", "R", "N", "F", "L", "P", "Z", "K", "W", "M", "J", "V", "C",
                 "O", "Y", "Q", "X", "H", "B", "A", "U", "G", "I", "D", "S", "E"
             ]),
-            "rotor_VI": deque([
+            "VI": deque([
                 "E", "K", "S", "T", "D", "H", "Y", "B", "N", "Z", "O", "R", "X",
                 "G", "L", "J", "A", "W", "U", "C", "F", "M", "P", "I", "V", "Q"
             ]),
-            "rotor_VII": deque([
+            "VII": deque([
                 "X", "B", "Q", "W", "A", "J", "Y", "P", "G", "O", "N", "H", "U",
                 "S", "T", "D", "V", "E", "M", "C", "R", "K", "L", "I", "Z", "F"
             ]),
-            "rotor_VIII": deque([
+            "VIII": deque([
                 "J", "O", "C", "F", "R", "L", "A", "N", "H", "T", "S", "U", "G",
                 "E", "B", "W", "V", "Z", "M", "X", "K", "I", "P", "Y", "D", "Q"
             ]),
@@ -71,6 +75,11 @@ class Enigma:
             ],
         }
 
+    def set_rotors(self):
+        self.enigma_rotors["right"] = self.rotors[self.user_rotors[0]]
+        self.enigma_rotors["center"] = self.rotors[self.user_rotors[1]]
+        self.enigma_rotors["left"] = self.rotors[self.user_rotors[2]]
+
 
 if __name__ == "__main__":
-    ...
+    enigma = Enigma("ABC", "XYZ")

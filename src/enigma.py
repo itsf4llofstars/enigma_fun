c4d4cd96672d4870023c807d511b2b77dcd7e7de
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-19 23:21:27 -0500

    Add Enigma class

--- src/enigma.py
+++ src/enigma.py
@@ -1,4 +1,16 @@
-"""enigma.py"""
+"""enigma.py
+Enigma Machine class file
+"""
+
+
+class Enigma:
+    """Enigma Machine class"""
+
+    def __init__(self):
+        self.keyboard = [
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        ]
 
 
 if __name__ == "__main__":

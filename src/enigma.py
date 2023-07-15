01a3141cbe0b466cafa664a72d784627946c109d
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-14 20:24:56 -0500

    Correct spelling form to from

--- src/enigma.py
+++ src/enigma.py
@@ -8,7 +8,7 @@ class Enigma:
     """Enigma Machine class"""
 
     def __init__(self, msg_key: str, day_key: str, user_rotors=None):
-        # Conditional auto generated form PyCharm
+        # Conditional auto generated from PyCharm
         if user_rotors is None:
             user_rotors = ["I", "II", "III"]
 
@@ -22,60 +22,60 @@ class Enigma:
         self.keyboard = deque([
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-        ])
+            ])
 
         self.rotors = {
-            "I": deque([
-                "U", "L", "T", "A", "O", "P", "I", "Q", "F", "Z", "Y", "N", "R",
-                "M", "B", "E", "V", "X", "W", "H", "G", "D", "J", "K", "S", "C"
-            ]),
-            "II": deque([
-                "N", "Y", "L", "S", "V", "I", "M", "T", "C", "F", "Z", "G", "Q",
-                "K", "E", "R", "H", "U", "X", "B", "P", "A", "D", "J", "O", "W"
-            ]),
-            "III": deque([
-                "P", "R", "X", "Z", "A", "F", "L", "C", "O", "M", "E", "N", "S",
-                "D", "Q", "W", "J", "T", "U", "B", "K", "Y", "H", "I", "V", "G"
-            ]),
-            "IV": deque([
-                "L", "A", "P", "M", "V", "Q", "H", "S", "U", "G", "B", "J", "T",
-                "C", "I", "D", "Y", "F", "W", "E", "O", "Z", "R", "N", "X", "K"
-            ]),
-            "V": deque([
-                "T", "R", "N", "F", "L", "P", "Z", "K", "W", "M", "J", "V", "C",
-                "O", "Y", "Q", "X", "H", "B", "A", "U", "G", "I", "D", "S", "E"
-            ]),
-            "VI": deque([
-                "E", "K", "S", "T", "D", "H", "Y", "B", "N", "Z", "O", "R", "X",
-                "G", "L", "J", "A", "W", "U", "C", "F", "M", "P", "I", "V", "Q"
-            ]),
-            "VII": deque([
-                "X", "B", "Q", "W", "A", "J", "Y", "P", "G", "O", "N", "H", "U",
-                "S", "T", "D", "V", "E", "M", "C", "R", "K", "L", "I", "Z", "F"
-            ]),
-            "VIII": deque([
-                "J", "O", "C", "F", "R", "L", "A", "N", "H", "T", "S", "U", "G",
-                "E", "B", "W", "V", "Z", "M", "X", "K", "I", "P", "Y", "D", "Q"
-            ]),
-        }
+                "I": deque([
+                    "U", "L", "T", "A", "O", "P", "I", "Q", "F", "Z", "Y", "N", "R",
+                    "M", "B", "E", "V", "X", "W", "H", "G", "D", "J", "K", "S", "C"
+                    ]),
+                "II": deque([
+                    "N", "Y", "L", "S", "V", "I", "M", "T", "C", "F", "Z", "G", "Q",
+                    "K", "E", "R", "H", "U", "X", "B", "P", "A", "D", "J", "O", "W"
+                    ]),
+                "III": deque([
+                    "P", "R", "X", "Z", "A", "F", "L", "C", "O", "M", "E", "N", "S",
+                    "D", "Q", "W", "J", "T", "U", "B", "K", "Y", "H", "I", "V", "G"
+                    ]),
+                "IV": deque([
+                    "L", "A", "P", "M", "V", "Q", "H", "S", "U", "G", "B", "J", "T",
+                    "C", "I", "D", "Y", "F", "W", "E", "O", "Z", "R", "N", "X", "K"
+                    ]),
+                "V": deque([
+                    "T", "R", "N", "F", "L", "P", "Z", "K", "W", "M", "J", "V", "C",
+                    "O", "Y", "Q", "X", "H", "B", "A", "U", "G", "I", "D", "S", "E"
+                    ]),
+                "VI": deque([
+                    "E", "K", "S", "T", "D", "H", "Y", "B", "N", "Z", "O", "R", "X",
+                    "G", "L", "J", "A", "W", "U", "C", "F", "M", "P", "I", "V", "Q"
+                    ]),
+                "VII": deque([
+                    "X", "B", "Q", "W", "A", "J", "Y", "P", "G", "O", "N", "H", "U",
+                    "S", "T", "D", "V", "E", "M", "C", "R", "K", "L", "I", "Z", "F"
+                    ]),
+                "VIII": deque([
+                    "J", "O", "C", "F", "R", "L", "A", "N", "H", "T", "S", "U", "G",
+                    "E", "B", "W", "V", "Z", "M", "X", "K", "I", "P", "Y", "D", "Q"
+                    ]),
+                }
 
         self.enigma_rotors = {
-            "right": [deque([
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+                "right": [deque([
+                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
                     deque([])
-            ],
-            "center": [deque([
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
-                deque([])
-            ],
-            "left": [deque([
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
-                deque([])
-            ],
-        }
+                    ],
+                "center": [deque([
+                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+                    deque([])
+                    ],
+                "left": [deque([
+                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+                    deque([])
+                    ],
+                }
         # fmt: on
 
     def show_rotors(self):
